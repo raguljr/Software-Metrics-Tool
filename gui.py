272132f94d12de4bde2d80cea4cdeb36bd6bc29c
raguljr <jothiraj.ragul@gmail.com>
2016-02-15 20:00:38 +0530

    Create gui.py

---
+++ gui.py
@@ -0,0 +1,24 @@
+from Tkinter import *
+import tkFileDialog
+import frr
+root=Tk()
+e=StringVar();
+tx=StringVar();
+root.geometry("400x400+200+200")
+label = Label( root, text="*.c  or *.cpp formats only").grid(row=0,column=0)
+
+def bro():
+    file = tkFileDialog.askopenfile(parent=root,mode='rb',title='Choose a file')
+    e.set(file.name)
+
+def calc():
+    ty=logic.frr(e.get())
+    tx.set(''.join(ty))
+
+    
+E1 = Entry(root,textvariable=e).grid(row=1,column=0)
+B = Button(root, text ="Browse", command = bro).grid(row=1,column=1)
+B1 = Button(root, text ="Calculate",command=calc).grid(row=2,column=1)
+label1 = Label( root, textvariable=tx,text="res").grid(row=3,column=0)
+label2 = Label( root, text="Developed by Ragul J R").grid(row=4,column=0)
+root.mainloop()
